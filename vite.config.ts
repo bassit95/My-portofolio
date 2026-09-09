6a99d9bbe68285bdbfdfca476adbfd9320a9f8e6
bassit <bassitabimbola009@gmail.com>
2026-09-09 15:25:01 +0000

    first project

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,6 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
-   base: '/My-portofolio/',
   resolve: {
     tsconfigPaths: true,
    
